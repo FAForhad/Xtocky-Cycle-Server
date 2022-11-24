31d36ec2ab67c03c67bfd89f3894790393efc0ee
FAForhad <faforhad450@gmail.com>
2022-11-25 00:46:12 +0600

    single_category

--- index.js
+++ index.js
@@ -10,24 +10,39 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectID } = require('bson');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c3txqlb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
 
-    const allProductsCollection = client.db('XtockyCycle').collection('AllProducts')
     const categoriesCollection = client.db('XtockyCycle').collection('categories')
+    const allProductsCollection = client.db('XtockyCycle').collection('AllProducts')
 
     try {
-        app.get('/allproducts', async (req, res) => {
-            const query = {}
-            const result = await allProductsCollection.find(query).toArray();
-            res.send(result)
-        })
-
         app.get('/categories', async (req, res) => {
             const query = {}
             const result = await categoriesCollection.find(query).limit(3).toArray()
+            res.send(result)
+        })
+
+        app.get('/allcategories', async (req, res) => {
+            const query = {}
+            const result = await categoriesCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/allcategories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectID(id) }
+            const result = await categoriesCollection.findOne(query)
+            res.send(result)
+        })
+
+        // products
+        app.get('/allproducts', async (req, res) => {
+            const query = {}
+            const result = await allProductsCollection.find(query).toArray();
             res.send(result)
         })
     }
